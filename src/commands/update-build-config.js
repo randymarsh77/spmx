5134ac46c7ef25b254d3379dcfa723db834884af
Matthew Preucil <mattpsbs@gmail.com>
2017-04-25 22:46:06 -0700

    Mostly working config generation.
    - Still needs sequential updates, something is wrong there.

--- src/commands/update-build-config.js
+++ src/commands/update-build-config.js
@@ -2,9 +2,13 @@ import fs from 'fs';
 import github from 'github';
 import base64 from 'base-64';
 
-const git = github();
+const headers = {};
+if (process.env.GitHubToken) {
+	headers.Authorization = `token ${process.env.GitHubToken}`;
+}
+const git = github({ headers });
 
-function parseDependencies(owner) {
+function parsePackage(owner) {
 	const promise = new Promise((resolve, reject) => {
 		fs.readFile('Package.swift', 'utf8', (err, data) => {
 			if (err) reject(err);
@@ -13,6 +17,7 @@ function parseDependencies(owner) {
 	});
 	return promise
 		.then(text => {
+			const name = (new RegExp('name[ ]*:[ ]*"([a-zA-Z0-9]*)"')).exec(text)[1];
 			const dependencies = [];
 			[owner].forEach(x => {
 				const prefix = `https:\/\/github\.com\/${x}\/`;
@@ -26,14 +31,28 @@ function parseDependencies(owner) {
 					match = re.exec(text);
 				}
 			});
-			return dependencies;
+			return {
+				name,
+				dependencies,
+			};
 		});
 }
 
-function createConfig(owner, name) {
+function createConfig({ owner, repo, name, downstream }, expectedPath) {
+	console.log('Creating config at ', expectedPath);
 	return Promise.resolve({
 		name,
 		source: `https://github.com/${owner}/${name}`,
+		downstream,
+	}).then(config => git.repos.createFile({
+		owner,
+		repo,
+		path: expectedPath,
+		message: `[SWIFTX-BOT] Adding config for ${name}`,
+		content: base64.encode(JSON.stringify(config, null, '	')),
+	})).then(result => {
+		console.log('Created config for ', name);
+		console.log(result);
 	});
 }
 
@@ -42,31 +61,84 @@ function getBlobContent(request) {
 		.then(result => JSON.parse(base64.decode(result.data.content)));
 }
 
-function getConfigs({ dependencies, owner, configPath }) {
+function updateConfig({ owner, repo }, pkg, blobPath) {
+	console.log('Processing config update for ', blobPath);
+	return git.gitdata.getReference({ owner, repo, ref: 'heads/master' })
+		.then(head => git.gitdata.getTree({ owner, repo, sha: head.data.object.sha, recursive: true }))
+		.then(treeResult => treeResult.data.tree.find(x => x.path === blobPath))
+		.then(({ sha }) => getBlobContent({ owner, repo, sha }).then(config => ({ config, sha })))
+		.then(({ config, sha }) => {
+			const needsReference = pkg.dependencies.find(x => x.name === config.name);
+			const hasReference = config.downstream.find(x => x.name === pkg.name);
+			if (needsReference && !hasReference) {
+				console.log('  Adding reference...');
+				return {
+					config: {
+						...config,
+						downstream: [
+							...config.downstream,
+							{ name: pkg.name },
+						],
+					},
+					sha,
+				};
+			} else if (!needsReference && hasReference) {
+				console.log('  Removing reference...');
+				return {
+					config: {
+						...config,
+						downstream: config.downstream.filter(x => x.name !== pkg.name),
+					},
+					sha,
+				};
+			}
+			console.log('  Skipping...');
+			return {};
+		})
+		.then(({ config, sha }) => (config ? git.repos.updateFile({
+			owner,
+			repo,
+			path: blobPath,
+			message: `[SWIFTX-BOT] Adding ${pkg.name} as a dependency to ${config.name}`,
+			content: base64.encode(JSON.stringify(config, null, '	')),
+			sha,
+		}) : null))
+		.then(result => {
+			if (result) {
+				console.log('Updated config with ', pkg.name);
+			}
+		});
+}
+
+function processPackage({ pkg, owner, configPath }) {
+	// All configs in the repo, in case we need to remove downstream dependencies
+	// All dependencies, to create or update exsiting configs
+	// Processing must happen sequentially so we always deal with the most up-to-date refs
 	const pathComponents = configPath.split('/');
 	const repo = pathComponents[0];
 	const basePath = pathComponents.length > 1 ? pathComponents.slice(1).join('/') : '';
 	return git.gitdata.getReference({ owner, repo, ref: 'heads/master' })
 		.then(head => git.gitdata.getTree({ owner, repo, sha: head.data.object.sha, recursive: true }))
-		.then(treeResult => Promise.all(dependencies.map(x => {
-			const { name } = x;
-			const blobMeta = treeResult.data.tree.find(y => y.path === `${basePath}/${name}.json`);
-			return !blobMeta ?
-				createConfig(owner, name) :
-				getBlobContent({ owner, repo, sha: blobMeta.sha });
-		})));
+		.then(treeResult => treeResult.data.tree.filter(x => x.path.startsWith(basePath) && x.path.endsWith('.json')))
+		.then(existingConfigs => {
+			let updatePromise = Promise.resolve();
+			const processedConfigPaths = [];
+			existingConfigs.forEach(blobMeta => {
+				processedConfigPaths.push(blobMeta.path);
+				updatePromise = updatePromise
+					.then(() => updateConfig({ owner, repo }, pkg, blobMeta.path));
+			});
+			return updatePromise.then(() => processedConfigPaths);
+		})
+		.then(processedConfigPaths => {
+			const missingConfigs = pkg.dependencies.filter(x => !processedConfigPaths.find(y => y === `${basePath}/${x.name}.json`));
+			return Promise.all(missingConfigs.map(x => createConfig({ owner, repo, name: x.name, downstream: [{ name: pkg.name }] }, `${basePath}/${x.name}.json`)));
+		});
 }
 
 export default function updateBuildConfig({ owner, configPath }) {
-	return parseDependencies(owner)
-		.then(dependencies => getConfigs({ dependencies, owner, configPath }))
-		.then(configs => {
-			console.log('got configs: ', configs);
-			// TODO
-			// ... update them with downstream info, if needed
-			// ... get self config, update lastTriggered to ensure we have a commit
-			// ... commit all configs
-		})
+	return parsePackage(owner)
+		.then(pkg => processPackage({ pkg, owner, configPath }))
 		.then(() => ({
 			code: 0,
 		}));
